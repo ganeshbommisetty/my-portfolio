8caf6840c2d228756556d02644c1cc2171c8dc22
ganeshbommisetty <ganesh.bommisetty1998@gmail.com>
2025-04-17 21:41:47 -0500

    fixed the bug

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// the base will take the appropriate path when it run in github page and in local
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/',
   plugins: [react()],
